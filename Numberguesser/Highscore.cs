e1c1b067250aa20978b5052fdba937a9eb389aef
Mando-06 <110893098+Mando-06@users.noreply.github.com>
2023-09-15 20:34:58 +0200

    Add files via upload

---
+++ Numberguesser/Highscore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numberguesser_ILA2_1300
+{
+    internal class Highscore
+    {
+        private List<int> highscores = new List<int>();
+
+        public void AddScore(int score)
+        {
+            highscores.Add(score);
+        }
+
+        public void DisplayHighscores()
+        {
+            highscores.Sort();//Chat GPT
+            Console.WriteLine("Highscores:");
+
+            for (int i = 0; i < highscores.Count; i++)
+            {
+                Console.WriteLine($"Platz {i + 1}: {highscores[i]} Versuche");
+            }
+        }
+    }
+}
---
+++ Numberguesser/Program.cs
@@ -0,0 +1,72 @@
+﻿using Numberguesser_ILA2_1300;
+using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Highscore highscore = new Highscore();
+            StartGame(highscore);
+        }
+
+        static void StartGame(Highscore highscore)
+        {
+            do
+            {
+                int secretNumber = new Random().Next(1, 101);
+                int attempts = 0;
+                bool hasGuessedCorrectly = false;
+
+                Console.WriteLine("Willkommen beim Numberguesser");
+                Console.WriteLine("Es wurde eine Geheimezahl zwischen 1 und 100 generiert, versuche sie zu erraten :)");
+
+                while (!hasGuessedCorrectly)
+                {
+                    Console.Write("Gib eine Zahl ein: ");
+                    string input = Console.ReadLine();
+
+                    if (!int.TryParse(input, out int guessedNumber))
+                    {
+                        Console.WriteLine("Ungültige Eingabe. Bitte gib eine gültige Zahl ein.");
+                        continue;
+                    }
+
+                    attempts++;
+
+                    if (guessedNumber < secretNumber)
+                    {
+                        Console.WriteLine("Die Geheimzahl ist größer als deine Eingabe.");
+                    }
+                    else if (guessedNumber > secretNumber)
+                    {
+                        Console.WriteLine("Die Geheimzahl ist kleiner als deine Eingabe.");
+                    }
+                    else
+                    {
+                        hasGuessedCorrectly = true;
+                        Console.WriteLine($"Glückwunsch! Du hast die Geheimzahl {secretNumber} in {attempts} Versuchen erraten.");
+                        highscore.AddScore(attempts);
+                        highscore.DisplayHighscores();
+                    }
+                }
+
+                string playAgain;
+                do
+                {
+                    Console.Write("Möchtest du nochmal spielen? (ja/nein): ");
+                    playAgain = Console.ReadLine()?.ToLower(); // Chat GPT
+                } while (playAgain != "ja" && playAgain != "nein");
+
+                if (playAgain == "nein")
+                {
+                    Console.WriteLine("Danke fürs Spielen!");
+                    break;
+                }
+
+
+            } while (true);
+        }
+    }
+}
